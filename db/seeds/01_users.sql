4ea6902f99cbb810ab7f8b25661ede184fe65aa1
Brad Ruud <bradruud@gmail.com>
2020-04-04 23:02:36 +0000

    split schema and seeds into seperate files

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,13 @@
--- Users table seeds here (Example)
--- INSERT INTO users (name) VALUES ('Alice');
--- INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, location)
+VALUES
+('Joe Exotic', 'Oklahoma'),
+('Leonidas Lichtfoth', 'Richmond'),
+('Philly Fulstow', 'Burnaby'),
+('Eamon Moreby', 'Surrey'),
+('Rochelle Douris', 'Langley'),
+('Aurelea Enga', 'Abbotsford'),
+('Dehlia Bellie', 'Vancouver'),
+('Phedra Hilldrop', 'Vancouver'),
+('Lianne Bulle', 'Surrey'),
+('Kaila Napoleone', 'Burnaby'),
+('Noelyn Jaskowicz', 'Richmond');
--- db/seeds/02_listings.sql
+++ db/seeds/02_listings.sql
@@ -1,17 +1,3 @@
-INSERT INTO users (name, location)
-VALUES
-('Joe Exotic', 'Oklahoma'),
-('Leonidas Lichtfoth', 'Richmond'),
-('Philly Fulstow', 'Burnaby'),
-('Eamon Moreby', 'Surrey'),
-('Rochelle Douris', 'Langley'),
-('Aurelea Enga', 'Abbotsford'),
-('Dehlia Bellie', 'Vancouver'),
-('Phedra Hilldrop', 'Vancouver'),
-('Lianne Bulle', 'Surrey'),
-('Kaila Napoleone', 'Burnaby'),
-('Noelyn Jaskowicz', 'Richmond');
-
 
 INSERT INTO listings (user_id, title, description, image, date_created, price, behaviour, category)
 VALUES
