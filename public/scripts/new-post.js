ab369a51aac992d0b75708b27ddc6988c9ea0555
Brad Ruud <bradruud@gmail.com>
2020-04-08 21:14:35 +0000

    update to new-post.js

--- public/scripts/new-post.js
+++ public/scripts/new-post.js
@@ -1,16 +1,33 @@
-$(document).ready(function(){
 
-  require("dotenv").config({ path: "../../" });
+const newPost = function(){
 
-  const config = require("../../lib/db");
-  const pg = require("pg");
-  const db = new pg.Client(config);
+  let queryValues = [];
 
-  db.connect((err) => {
-    if (err) throw err;
-    console.log("connected to server");
-  });
+  const title = $('#title').val();
+  const description = $('#description').val();
+  const price = $('#price').val();
+  const postUrl = $('#post-url').val();
+  const behaviour = $('#behaviour').val();
+  const category = $('#category').val();
+
+  queryValues.push(title, description, price, postUrl, behaviour, category);
+
+  const queryString = `INSERT INTO listings (title, description, price, postUrl, behaviour, category) VALUES ($1, $2, $3, $4, $5, $6);`;
+
+  console.log(queryString, queryValues);
+
+  alert("I'm a ready shortcut");
+  return db.query(queryString, queryValues);
+
+}
+// const testPost = function(){
+//   console.log("testPost working??");
+
+// }
+$(function(){
+  // console.log("we are on ready");
 
 
+  $( "#new-post-submit" ).on( "click", newPost );
 
 });
